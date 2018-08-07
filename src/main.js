3a8189dd0979e00c5884e9d3b6f2c5a57d955a12
baihe <baihe773373@126.com>
2018-08-07 12:51:25 +0800

    提交header

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入全局样式
+// 导入bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
+// 导入index.css
 import './assets/css/index.css';
 
 new Vue({
